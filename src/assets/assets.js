c7e1ebd0a8c1a92ed0cbaeac10f0c4a3d7174a56
KushalAgrawal30 <kushalagrawal3011@gmail.com>
2025-04-04 03:48:58 +0530

    update css

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -13,8 +13,10 @@ import message_icon from './message_icon.png'
 import code_icon from './code_icon.png'
 import send_icon from './send_icon.png'
 import gemini_icon from './gemini_icon.png'
+import gemini_logo from './gemini-color.svg'
 
 export const assets = {
+    gemini_logo,
     history_icon,
     menu_icon,
     plus_icon,
